bb655d8176b91dbab3035b620d3bd9dd30ed3c86
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-07-13 19:10:50 +0300

    feat: layout wrapper
    Create Layout wrapper that will be used as the common layout for all
    pages in the application. This will be used by the application to set a
    default Head component that will have meta data for the application.
    
    This is a basic stateless React component that is passed to props
    1. children : which is any of the types, object, element, array, node,
    element
    2. pageTitle: which is the title of the page

---
+++ components/Layout.jsx
@@ -0,0 +1,32 @@
+/**
+ * Base Layout for all pages in the application
+ */
+
+import React, { Fragment } from 'react';
+import { node, object, element, oneOfType, array, string} from "prop-types";
+import Head from "next/head";
+
+/**
+ * This is a Layout component that will wrap the whole application. This will be used to define a default and common Layout
+ * used in the enter application. Each page will have this layout 
+ * @param {String} pageTitle Page title that will be used in the Head
+ * @param {Object} children Children props passed to Layout
+ */
+const Layout = (children, pageTitle) => {
+    <Fragment>
+        <Head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
+            <title>{pageTitle || "RealTime Chat"}</title>
+        </Head>
+        {children}
+    </Fragment>
+}
+
+Layout.propTypes = {
+    pageTitle: string,
+    children: oneOfType([ object, element, node, array])
+}
+
+export default Layout;
